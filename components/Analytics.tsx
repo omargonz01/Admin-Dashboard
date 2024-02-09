5b8bf49c2b0fde9591476e8817105c8482b1490b
Omar Gonzalez <gomarzalez@gmail.com>
2024-02-08 20:04:30 -0800

    build out dash analytics

--- components/Analytics.tsx
+++ components/Analytics.tsx
@@ -1,5 +1,8 @@
 "use client";
 
+import Chart from "@/components/Chart";
+import { BarList, Card, Flex, Grid, Metric, Text, Title } from "@tremor/react";
+
 const website = [
   { name: "/home", value: 1230 },
   { name: "/contact", value: 751 },
@@ -42,5 +45,35 @@ const data = [
 ];
 
 export default function Analytics() {
-  return <main>analytics</main>;
+  return (
+    <main className="p-4 md:p-10 mx-auto max-w-7xl">
+      <Grid numItemsSm={2} numItemsLg={3} className="gap-6">
+        {data.map((item) => (
+          <Card key={item.category}>
+            <Title>{item.category}</Title>
+            <Flex
+              justifyContent="start"
+              alignItems="baseline"
+              className="space-x-2"
+            >
+              <Metric>{item.stat}</Metric>
+              <Text>Total views</Text>
+            </Flex>
+            <Flex className="mt-6">
+              <Text>Pages</Text>
+              <Text className="text-right">Views</Text>
+            </Flex>
+            <BarList
+              data={item.data}
+              valueFormatter={(number: number) =>
+                Intl.NumberFormat("us").format(number).toString()
+              }
+              className="mt-2"
+            />
+          </Card>
+        ))}
+      </Grid>
+      <Chart />
+    </main>
+  );
 }
