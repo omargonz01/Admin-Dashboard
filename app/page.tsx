f31040e024164c96f0d664150d1be8ba606aff03
Omar Gonzalez <gomarzalez@gmail.com>
2024-02-12 22:11:01 -0800

    build async Home function

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,21 @@
-export default function Home() {
-  return <main>This is where my home/main content will be </main>;
+type Props = {
+  searchParams: {
+    q?: string;
+  };
+};
+
+export default async function Home({ searchParams }: Props) {
+  const query = searchParams.q;
+  const users = await prisma.user.findMany({
+    where: {
+      name: {
+        contains: query,
+        mode: "insensitive",
+      },
+      email: {
+        contains: query,
+        mode: "insensitive",
+      },
+    },
+  });
 }
